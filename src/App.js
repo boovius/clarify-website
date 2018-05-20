2ec85b4b2a54eafde08c2cb63ec02f533bc4a0ea
Joshua Book <joshua.book@gmail.com>
2018-05-19 20:46:58 -0400

    make app functional component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from './components/LandingPage/LandingPage';
 import DayOne from './components/DayOne/DayOne';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/day-1" component={DayOne} />
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/day-1" component={DayOne} />
+    </div>
+  </Router>
+);
 
 export default App;
