675a9c87bbc0e3ad2a1ad272799cf6c2755dfde9
Joshua Book <joshua.book@gmail.com>
2018-05-23 22:46:12 -0400

    mount delay message components on linear flow cards with transition fade in

--- src/components/LinearFlowCard.js
+++ src/components/LinearFlowCard.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import DownArrowLink from './DownArrowLink'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import Delayed from 'react-delayed'
 
 const LinearFlowCard = ({day, index, messages}) => (
   <div className={`${day}-${index}`}>
-    {messages.map((message,i) => <p key={i}>{message}</p>)}
+    {messages.map((message,i) => (
+      <Delayed key={i} mounted={true} mountAfter={(i+0)*500}>
+        <ReactCSSTransitionGroup
+          transitionName='message'
+          transitionAppearTimeout={500}
+          transitionAppear={true}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <p>{message}</p>
+        </ReactCSSTransitionGroup>
+      </Delayed>
+    ))}
     <DownArrowLink next={`${parseInt(index)+1}`} />
   </div>
 )
