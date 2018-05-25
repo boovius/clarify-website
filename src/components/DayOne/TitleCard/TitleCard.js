133c71b95e347ee670f504b9c48f9f48b30d73e3
Joshua Book <joshua.book@gmail.com>
2018-05-24 22:58:25 -0400

    fixups on wording and flow

--- src/components/DayOne/TitleCard/TitleCard.js
+++ src/components/DayOne/TitleCard/TitleCard.js
@@ -7,7 +7,6 @@ const TitleCard = ({match}) => (
   <div className='title-card'>
     <p>Day 1</p>
     <h2>Begin with the End</h2>
-    <DelayedWord word="stuck?" />
     <DownArrowLink next='2' />
   </div>
 )
--- src/components/DayOne/cards.js
+++ src/components/DayOne/cards.js
@@ -7,36 +7,45 @@ const cards = [
     "It requires that we ask ourselves hard questions.",
   ],
   [
+    "Questions like:",
     "What really matters to me?",
-    "What matters most?",
+    "What really matters most?",
   ],
   [
     "We need to consider the totality of our lives.",
+  ],
+  [
     "Our lives as a whole.",
   ],
   [
-    "To do this, we're going to imagine our lives at the very end of them.",
+    "So to do this,",
+    "we're going to imagine our lives at the very end of them.",
   ],
   [
     "We're going to blast forward many, many years",
-    "After you have lived a long, long and beautiful life",
+  ],
+  [
+    "After you have lived a long and beautiful life.",
   ],
   [
     "You find yourself at your funeral",
   ],
   [
-    "You're able to walk and move around and observe what people are saying",
+    "You're able to walk, move around, and observe what people are saying",
   ],
   [
     "A dear loved one gets up and gives a beautiful eulogy for you",
-    "Then a dear co-worker gets up, a partner of yours in amazing business and gives another incredible eulogy for you",
   ],
   [
     "You cannot believe what you're hearing",
   ],
   [
     "It seems you've lived your greatest life",
-    "You've accomplished all the the things you have dreamed of",
+    "You've accomplished all the things you would dream of for your life",
+  ],
+  [
+    "Then a dear co-worker gets up,",
+    "a partner of yours in amazing business and gives another incredible eulogy for you",
   ],
   [
     "So ...",
@@ -52,9 +61,11 @@ const cards = [
     "As you move around the funeral, what are people saying about you?",
   ],
   [
-    "What was most memorable about you?",
-    "What was the lasting impression you left upon people; the lasting impression of how you lived your life?",
     "How did you effect people's lives?"
+  ],
+  [
+    "What was the lasting impression you left upon people;",
+    "the lasting impression of how you lived your life?",
   ],
 ]
 
