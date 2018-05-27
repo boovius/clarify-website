382b881d9f14a780c335caf3b15fb781144d15d9
Joshua Book <joshua.book@gmail.com>
2018-05-27 16:46:27 -0400

    make starting link in day 1 title card a round button

--- src/components/DayOne/TitleCard/TitleCard.js
+++ src/components/DayOne/TitleCard/TitleCard.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import DownArrowLink from '../../DownArrowLink'
-import DelayedWord from '../../DelayedWord'
+import RoundButton from '../../RoundButton'
+import { primaryColor, secondaryColor } from '../DayOne.styles'
+import { Link } from 'react-router-dom'
 
 
 const TitleCard = ({match}) => (
   <div className='title-card'>
     <p>Day 1</p>
     <h2>Begin with the End</h2>
-    <DownArrowLink next='2' />
+    <Link to='2'>
+      <RoundButton
+        primaryColor={primaryColor}
+        secondaryColor={secondaryColor}
+      >start</RoundButton>
+    </Link>
   </div>
 )
 
