4366e2954c7409690ad67408f0ff379b99d83fa2
Joshua Book <joshua.book@gmail.com>
2018-05-27 16:47:54 -0400

    modify day1 parent component to go from title to card flow

--- src/components/DayOne/DayOne.js
+++ src/components/DayOne/DayOne.js
@@ -2,17 +2,15 @@ import React from 'react'
 import { Route, Switch, Redirect } from "react-router-dom"
 import Container from '../Container/Container'
 import TitleCard from './TitleCard/TitleCard'
-import OneTwo from './OneTwo/OneTwo'
-import OneThree from './OneThree/OneThree'
 import LinearFlowCard from '../LinearFlowCard'
 import './DayOne.css'
 import cards from './cards'
 
 
 const cardFlow = (match) => cards.map((messages, cardIndex) =>
-  <Route key={cardIndex} path={`${match.url}/${4 + cardIndex}`} render={
+  <Route key={cardIndex} path={`${match.url}/${2 + cardIndex}`} render={
     (props) => (
-      <LinearFlowCard {...props} flowLength={cards.length} day='one' index={`${4 + cardIndex}`} messages={messages} />
+      <LinearFlowCard {...props} flowLength={cards.length} day='one' index={`${2 + cardIndex}`} messages={messages} />
     )
   }/>
 )
@@ -22,8 +20,6 @@ const DayOne = ({match}) => (
     <Container>
       <Switch>
         <Route path={`${match.url}/1`} component={TitleCard} />
-        <Route path={`${match.url}/2`} component={OneTwo} />
-        <Route path={`${match.url}/3`} component={OneThree} />
         {cardFlow(match)}
         <Redirect to={`${match.url}/1`} />
       </Switch>
