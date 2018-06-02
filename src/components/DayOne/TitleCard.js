efb6243c080e7619094755b3ad94b72fd873ee92
Joshua Book <joshua.book@gmail.com>
2018-06-02 17:55:14 -0400

    add delayedfadeinset and implement in day 1 title card

--- src/components/DayOne/TitleCard.js
+++ src/components/DayOne/TitleCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from '../Container'
+import DelayedFadeInSet from '../DelayedFadeInSet'
 import RoundButton from '../RoundButton'
 import { primaryColor, secondaryColor } from './DayOne.styles'
 import { Link } from 'react-router-dom'
@@ -8,14 +9,16 @@ import { Link } from 'react-router-dom'
 const TitleCard = ({match}) => (
   <Container>
     <div className='title-card'>
-      <p>Level One</p>
-      <h2>Begin with the End</h2>
-      <Link to='2'>
-        <RoundButton
-          primaryColor={primaryColor}
-          secondaryColor={secondaryColor}
-        >start</RoundButton>
-      </Link>
+      <DelayedFadeInSet index={0} transition='message' timeout={500}>
+        <p>Level One</p>
+        <h2>Begin with the End</h2>
+        <Link to='2'>
+          <RoundButton
+            primaryColor={primaryColor}
+            secondaryColor={secondaryColor}
+          >start</RoundButton>
+        </Link>
+      </DelayedFadeInSet>
     </div>
   </Container>
 )
